a67e9e9081cf73fb42dbcdcf6c316a781916204c
rishab99058 <rishab99058@gmail.com>
2025-10-26 03:21:45 +0530

    .\api-gateway\

---
+++ api-gateway/src/test/java/com/rishab99058/linkedIn/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishab99058.linkedIn.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
