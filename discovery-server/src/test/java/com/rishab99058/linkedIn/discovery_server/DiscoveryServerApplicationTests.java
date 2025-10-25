94d48daebf8e1f4e3dce9c72d41e8c5ea6fbe473
rishab99058 <rishab99058@gmail.com>
2025-10-25 11:04:46 +0530

    .\Eureka Server

---
+++ discovery-server/src/test/java/com/rishab99058/linkedIn/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rishab99058.linkedIn.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
